dbbacd9d04e9baec05b08d5a66034fe3d6a1e991
VolodymyrHalchyk <ckatbiha1991@gmail.com>
2021-07-08 16:41:56 +0300

    Lesson_3_2

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -2,7 +2,7 @@ export default function User({item}) {
 
     return(
         <div>
-            {item.id} - {item.name} - {item.email}
+            {item.id} - {item.name} - {item.email} - <button onClick={}>show Posts</button>
         </div>
     )
 }
