e44663048efdf95d0e73bb93d2fbe62402b6ae73
VolodymyrHalchyk <ckatbiha1991@gmail.com>
2021-07-29 11:22:28 +0300

    Lesson_4_1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div>
+    return (
+        <Router>
+            <div>
+                <Link to={'/'}>Home page</Link>
+                <Link to={'/users'}>users</Link>
+            </div>
 
-    </div>
-  );
+            <Switch>
+                <Route />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
